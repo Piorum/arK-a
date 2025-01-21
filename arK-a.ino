52572373d1edc8bd3c61383a149356b03226319f
Piorum <90207649+Piorum@users.noreply.github.com>
2025-01-21 09:31:45 -0800

    Add files via upload

--- arK-a.ino
+++ arK-a.ino
@@ -10,14 +10,14 @@ void loop() {
   static bool active = false;
 
   if (Serial.available() > 0) {
-    String incoming = Serial.readStringUntil('\n');
-    incoming.trim();
+    String data = Serial.readStringUntil('\n');
+    data.trim();
 
-    if (incoming == "arK?") {
+    if (data == "arK?") {
       Serial.println("arK!");
       active = true;
     }
-    if (incoming == "arK/") {
+    if (data == "arK/") {
       active = false;
     }
 
@@ -25,15 +25,74 @@ void loop() {
       return;
     }
 
-    if (incoming.length() < 2) {
+    if (data.length() < 1) {
       return;
     }
 
-    else if (incoming[0] == 'D'){
-      Keyboard.press(incoming[1]);
+    //K(S/N)(KEY)(D/U)
+    //M
+    switch(data[0]){
+      case 'K':
+        KeyboardHandler(data);
+        break;
+      case 'M':
+        MouseHandler(data);
+        break;
     }
-    else if (incoming[0] == 'U'){
-      Keyboard.release(incoming[1]);
-    }
+
+  }
+}
+
+void KeyboardHandler(String data){
+  if (data.length() < 4) {
+    return;
+  }
+
+  uint8_t key;
+
+  switch(data[1]){
+
+    //special key
+    case 'S':
+      key = GetSpecialKey(data[2]);
+
+      //sentinel value for no case found
+      if (key == 0) {
+        return;
+      }
+
+      break;
+
+    case 'N':
+      key = static_cast<uint8_t>(data[2]);
+
+      break;
+  }
+
+  //normal key
+  switch(data[3]){
+    case 'D':
+      Keyboard.press(key);
+      break;
+    case 'U':
+      Keyboard.release(key);
+      break;
+  }
+}
+
+uint8_t GetSpecialKey(char index){
+  //(T=Tab)
+  switch(index){
+    case 'T':
+      return KEY_TAB;
+      break;
+    default:
+      return 0;
+  }
+}
+
+void MouseHandler(String data){
+  switch(data[1]){
+
   }
 }
